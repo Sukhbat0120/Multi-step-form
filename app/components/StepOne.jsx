c77b94a4c094b00ead430501c137ef1a48de7e41
25LP2243 25LP2243 <25LP2243@G303-CC0139.local>
2025-09-24 12:37:29 +0800

    currentInex

--- app/components/StepOne.jsx
+++ app/components/StepOne.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import { Button } from "./Button";
 import { Input } from "./Input";
 
 export function StepOne() {
+  const [errors, setErrors] = useState({
+    FirstName: null,
+    LastName: null,
+    UserName: null,
+  });
+  const onClickButton = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(data.get("text"));
+    console.log(event.target.FirstName.value);
+    if (event.target.FirstName.value.lenght < 1) {
+      setErrors({ ...errors, FirstName: "too short" });
+    }
+    if (event.target.LastName.value.lenght < 1) {
+      setErrors({ ...errors, LastName: "too short" });
+    }
+    console.log(event.target.LastName.value);
+    if (event.target.UserName.value.lenght < 1) {
+      setErrors({ ...errors, UserName: "too short" });
+    }
+    console.log(event.target.UserName.value);
+  };
+
   return (
     <>
       <div className="w-fit h-fit p-8 rounded-[8px] bg-gray-600  ">
@@ -14,19 +38,50 @@ export function StepOne() {
         <p className="text-[18px] text-gray-400 ">
           Please provide all current information accurately.
         </p>
-        <div className="flex flex-col">
-          <label htmlFor="">First name</label>
-          <Input></Input>
-          <label htmlFor="">Last name</label>
-          <Input></Input>
-          <label htmlFor="">Username</label>
-          <Input></Input>
-        </div>
+        <form onSubmit={onClickButton}>
+          <div className="flex flex-col">
+            <label htmlFor="" className="">
+              First name <span className="text-red-500">*</span>
+            </label>
+            <input
+              name="FirstName"
+              placeholder="Write here..."
+              type="text"
+              className="w-[87px] h-[20px] rounded-md border-2 border-solid border-black w-[366px] mt-2 mb-1 h-[48px] "
+            />
+            {errors.FirstName && (
+              <p className="text-red-500">{errors.FirstName}</p>
+            )}
+            <label htmlFor="">
+              Last name <span className="text-red-500">*</span>
+            </label>
+            <input
+              name="LastName"
+              placeholder="Write here..."
+              type="text"
+              className="w-[87px] h-[20px] rounded-md border-2 border-solid border-black w-[366px] mt-2 mb-1 h-[48px] "
+            />
+            <label htmlFor="">
+              Username <span className="text-red-500">*</span>
+            </label>
+            <input
+              name="UserName"
+              placeholder="Write here..."
+              type="text"
+              className="w-[87px] h-[20px] rounded-md border-2 border-solid border-black w-[366px] mt-2 mb-1 h-[48px] "
+            />
+          </div>
 
-        <div className="flex gap-6 mt-6">
-          <Button>Back</Button>
-          <Button>Continue</Button>
-        </div>
+          <div className="flex gap-6 mt-6">
+            <button
+              type="submit"
+              className="w-full h-[44px] p-2 rounded-md font-semibold bg-sky-500 hover:bg-sky-700 "
+            >
+              {currentIndex + 1}
+              Continue
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import { StepOne } from "./components/StepOne";
 import { StepTwo } from "./components/StepTwo";
 import { StepThree } from "./components/StepThree";
+import { useState } from "react";
 export default function Home() {
+  const CurrentStep = [StepOne, StepTwo, StepThree](currentIndex);
+
   return (
     <div className="flex pt-50 place-content-center">
-      <StepOne />
+      <CurrentStep currentIndex={currentIndex} />
     </div>
   );
 }
